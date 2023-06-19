16058faa918dc3b915dd814510262b202413d14b
Plamen Kamenov <112328223+pako8101@users.noreply.github.com>
2023-06-19 14:06:09 +0300

    Add files via upload

---
+++ Encapsulation/EncapsulationShoppingSpree/Main.java
@@ -0,0 +1,11 @@
+package EncapsulationShoppingSpree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] buyers = scanner.nextLine().split("=");
+
+    }
+}
---
+++ Encapsulation/EncapsulationShoppingSpree/Product.java
@@ -0,0 +1,37 @@
+package EncapsulationShoppingSpree;
+
+public class Product {
+private String name;
+private double cost;
+
+    public Product(String name, double cost) {
+      setName(name);
+      setCost(cost);
+    }
+
+    public void setName(String name) {
+        if (!name.trim().isEmpty()){
+            this.name = name;
+        }else {
+            throw  new IllegalArgumentException("Name cannot be empty");
+        }
+
+    }
+
+    public void setCost(double cost) {
+        if (cost>=0){
+            this.cost = cost;
+        }else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
